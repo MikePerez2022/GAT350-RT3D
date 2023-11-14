a63e1268ffd2b71056f50555feff6d1ec935d6d4
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2023-11-13 20:46:09 -0700

    Added Fantasy, world07, and refactored camcomp
    I am rendering a new scene with buildings.
    
    Had some trouble. It was the emissive and i fixed it

--- Source/Engine/Framework/Components/CameraComponent.cpp
+++ Source/Engine/Framework/Components/CameraComponent.cpp
@@ -21,10 +21,17 @@ namespace nc
 
 	void CameraComponent::Update(float dt)
 	{
-		// set view matrix with glm::lookAt function, use owner position
 		view = glm::lookAt(m_owner->transform.position, m_owner->transform.position + m_owner->transform.Forward(),  m_owner->transform.Up());
-		// set projection matrix with glm::perspective function (fov is in degrees, convert to radians)
-		projection = glm::perspective(glm::radians(fov), aspect, near, far);
+
+		if (projectionType == Perspective)
+		{
+			projection = glm::perspective(glm::radians(fov), aspect, near, far);
+		}
+		else
+		{
+			projection = glm::ortho(-size * aspect * 0.5f, size * aspect * 0.5f, -size * 0.5f, size * 0.5f, near, far);
+		}
+		
 
 	}
 
@@ -57,10 +64,14 @@ namespace nc
 
 	void CameraComponent::ProcessGui()
 	{
+		const char* types[] = { "Perspective", "Orthographic" };
+		ImGui::Combo("Projection", (int*)(&projectionType), types, 2);
+
 		ImGui::DragFloat("FOV", &fov, 0.1f);
 		ImGui::DragFloat("Aspect", &aspect, 0.1f);
 		ImGui::DragFloat("Near", &near, 0.1f);
 		ImGui::DragFloat("Far", &far, 0.1f);
+		ImGui::DragFloat("Size", &size, 0.1f);
 	}
 
 	void CameraComponent::Read(const json_t& value)
@@ -70,5 +81,12 @@ namespace nc
 		READ_DATA(value, aspect);
 		READ_DATA(value, near);
 		READ_DATA(value, far);
+
+
+		std::string projectionTypeName;
+		READ_NAME_DATA(value, "ProjectionType", projectionTypeName);
+		if (SUtils.IsEqualIgnoreCase("orthographic", projectionTypeName)) projectionType = Orthographic;
+
+		READ_DATA(value, size);
 	}
 }
